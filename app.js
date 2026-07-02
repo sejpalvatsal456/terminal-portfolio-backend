8ff519061d1f45916fe8f20f91a4a0415b5aa0bb
sejpalvatsal456 <sejpalvatsal456@gmail.com>
2026-07-02 11:26:37 +0530

    Give permission through cors.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import connectDB from './db/connect.js';
 import router from './routers/projectRouter.js';
+import cors from 'cors';
 
 const app = express();
 
 connectDB();
 app.use(express.json());
+app.use(cors());
 
 // app.get('/', (req, res) => {
 //   return res.send("Hello there");
