d5df64de0c4cb45d12899f394f492b694ed2941b
sejpalvatsal456 <sejpalvatsal456@gmail.com>
2026-07-03 20:29:26 +0530

    Added update and delete operation in project api

--- controllers/projectControllers.js
+++ controllers/projectControllers.js
@@ -13,3 +13,26 @@ export const insertProject = async(req, res) => {
   const newProject = await Project.insertOne({ name, link, desc });
   return res.status(200).json({ newProject });
 }
+
+export const updateProject = async(req, res) => {
+  const { id, name, link, desc } = req.body;
+  if (id.trim() === "") {
+    return res.status(500).json({ msg: "Missing id fields." });
+  }
+  if (name.trim() === "" || link.trim() === "" || desc.trim() === "") {
+    return res.status(500).json({ msg: "Missing all fields." });
+  }
+
+  const newUpdate = await Project.findByIdAndUpdate(id, { name: name, link: link, desc: desc }, { returnDocument: 'after' });
+  return res.status(200).json(newUpdate);
+}
+
+export const deleteProject = async(req, res) => {
+  const { id } = req.body;
+  const proj = await Project.findById(id);
+  if (!proj) {
+    return res.status(404).json({ msg: "There is no project with this id. " });
+  }
+  await Project.findByIdAndDelete(id);
+  return res.status(200).json({ msg: "Successfully deleted." });
+}
--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,9 +1,11 @@
 import e, { Router } from "express";
-import { getAllProjects, insertProject } from "../controllers/projectControllers.js";
+import { deleteProject, getAllProjects, insertProject, updateProject } from "../controllers/projectControllers.js";
 
 const router = Router();
 
 router.get('/', getAllProjects);
 router.post('/', insertProject);
+router.put('/', updateProject);
+router.delete('/', deleteProject);
 
 export default router;
